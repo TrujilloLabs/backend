6f1312bd65d34a5726e103f2a2550bd21b8319a6
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-11 09:08:45 -0500

    Add skeleton for createStoreWithAdmin method
    Introduces a new async method createStoreWithAdmin in StoresService for creating a store along with an admin user. The method is currently a placeholder and requires implementation.

--- src/modules/stores/stores.service.ts
+++ src/modules/stores/stores.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { Store } from './entities/store.entity';
+import { User, UserRole } from '../users/entities/user.entity';
+import { License } from '../lincese/entities/lincese.entity';
 import { CreateStoreDto } from './dto/create-store.dto';
 import { UpdateStoreDto } from './dto/update-store.dto';
 
 @Injectable()
 export class StoresService {
+
+  constructor(private DataSource: DataSource) { }
+
+  async createStoreWithAdmin(createStore, createUser) {
+
+  }
+
   create(createStoreDto: CreateStoreDto) {
     return 'This action adds a new store';
   }
