2abc6ea1185e5567020c29283273f4c4d7e4357f
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-11 15:03:08 -0500

    add: de Stores en Auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -4,8 +4,10 @@ import { UpdateAuthDto } from './dto/update-auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+
+
+  async createStoreWithAdmin(createStore) {
+    return 'This action adds a new store with an admin user';
   }
 
   findAll() {
