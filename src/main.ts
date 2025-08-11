8f65c85cb188430c3c090809f766afa27b779bab
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-10 23:19:38 -0500

    Add global prefix and validation pipe to NestJS app
    Configured the application to use the 'api/v1' global prefix and enabled a global ValidationPipe with strict options for request validation and transformation. Also added a startup log message displaying the running port.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3000);
+// }
+// bootstrap();
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api/v1')
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true
+      }
+    })
+  )
+
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`App running on port http://localhost:${port}`)
 }
 bootstrap();
+
