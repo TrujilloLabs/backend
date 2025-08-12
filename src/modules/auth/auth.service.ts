2b712dd06f93dc49a41620d18ceae5e4092467bb
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-11 21:57:41 -0500

    Creando servicio de auth

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource } from 'typeorm';
+import * as bcrypt from 'bcryptjs';
+
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
 import { CreateStoreDto } from '../stores/dto/create-store.dto';
@@ -7,7 +9,6 @@ import { CreateStoreDto } from '../stores/dto/create-store.dto';
 import { Store } from '../stores/entities/store.entity';
 import { User } from '../users/entities/user.entity';
 import { Role } from '../users/enums/user-role.enum';
-// import { async } from '../seed/seed-superadmin';
 
 
 
@@ -19,30 +20,37 @@ export class AuthService {
 
   ) { }
 
+  // Crear admin de tienda default: email pattern o venir en DTO
+  private adminPassword = Math.random().toString(36).slice(-8);
+  private hashed = bcrypt.hash(this.adminPassword, 10);
 
-  async authCreateStore(createStoreDto: CreateStoreDto) {
+
+  async authCreateStore(createStoreDto) {
     const storeRepo = this.dataSource.getRepository(Store);
     const store = storeRepo.create({
       name: createStoreDto.name,
       logo_url: createStoreDto.logo_url,
       description: createStoreDto.description,
       email: createStoreDto.email,
-      password: createStoreDto.password,
+      password: await this.hashed, // Contraseña por defecto para el admin de la tienda
       // state: (createStoreDto.state as 'activa' | 'inactiva') ?? 'inactiva', // Usa 'inactiva' si no viene en el DTO
     });
+    const savedStore = await storeRepo.save(store);
+    console.log(`Contraseña generada para el admin de la tienda: ${this.adminPassword}`);
 
-    //crear usario con el email el password en name de store
     const userRepo = this.dataSource.getRepository(User),
       user = userRepo.create({
         name: createStoreDto.name,
         email: createStoreDto.email,
-        password: createStoreDto.password,
+        password: await this.hashed, // Contraseña por defecto para el admin de la tienda
         role: Role.ADMIN_TIENDA,
       });
-
-    const savedStore = await storeRepo.save(store);
     const savedUser = await this.dataSource.getRepository(User).save(user);
-
+    return {
+      store: savedStore,
+      user: savedUser,
+      message: `Tienda creada con éxito. Contraseña generada para el admin de la tienda: ${this.adminPassword}`,
+    };
   }
 
   findAll() {
