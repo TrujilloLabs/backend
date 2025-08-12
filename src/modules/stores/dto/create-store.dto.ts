fcb0413d12a42c30737935fa2fcb76f27120438a
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-11 21:58:05 -0500

    Modificacion tem,poral en DTO

--- src/modules/stores/dto/create-store.dto.ts
+++ src/modules/stores/dto/create-store.dto.ts
@@ -38,10 +38,10 @@ export class CreateStoreDto {
     email: string;
 
     //password
-    @IsNotEmpty({ message: 'La contraseña no puede estar vacía' })
-    @IsString({ message: 'La contraseña debe ser una cadena de texto' })
-    @MaxLength(150, { message: 'La contraseña no puede exceder los 150 caracteres' })
-    password: string;
+    // @IsNotEmpty({ message: 'La contraseña no puede estar vacía' })
+    // @IsString({ message: 'La contraseña debe ser una cadena de texto' })
+    // @MaxLength(150, { message: 'La contraseña no puede exceder los 150 caracteres' })
+    // password: string;
 
     @IsOptional()
     @IsEnum(['activa', 'inactiva'], { message: 'El estado debe ser "activa" o "inactiva"' })
