d154ace93b51005814fcc7725df49e3c32d0a180
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-13 22:48:11 -0500

    Adds findByEmail method to UsersService
    Adds a method to the UsersService to retrieve a user by their email address.
    
    This method queries the database for a user with the specified email
    and returns the user object. If no user is found, it throws a
    NotFoundException.

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { DataSource } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
+
 
 @Injectable()
 export class UsersService {
+
+  constructor(
+    private userModel: DataSource) {
+  }
+
+
+
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
@@ -22,5 +35,22 @@ export class UsersService {
 
   remove(id: number) {
     return `This action removes a #${id} user`;
+
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = await this.userModel.getRepository(User).findOne({ where: { email } });
+    if (!user) throw new NotFoundException(`User with email ${email} not found`);
+    return {
+      user_id: user.user_id,
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      telephone: user.telephone,
+      address: user.address,
+      registration_date: user.registration_date,
+      role: user.role,
+      store: user.store
+    };
   }
 }
