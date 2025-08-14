bed4a3bcdde1704105d9a16ad14ee426ca83dd8c
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-13 22:47:42 -0500

    Imports UsersModule into AuthModule
    Imports UsersModule into AuthModule to enable access
    to user-related services within the authentication module.
    This is necessary for authentication logic that relies on user data.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -4,11 +4,15 @@ import { AuthController } from './auth.controller';
 import { Type } from 'class-transformer';
 import { User } from '../users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
+    // Aquí solo se importan otros módulos
+    UsersModule,
     // TypeOrmModule.forFeature([User])
   ]
 })
