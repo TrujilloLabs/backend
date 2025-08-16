00e8788b52b05d95a48ed5d16d84801987a36b2d
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-15 19:08:23 -0500

    Adds JWT module for authentication
    Configures and integrates the JWT module for handling authentication.
    
    This includes setting the secret key (using an environment variable with a fallback) and defining the token expiration time.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { User } from '../users/entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from '../users/users.service';
 import { UsersModule } from '../users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
@@ -13,6 +14,10 @@ import { UsersModule } from '../users/users.module';
   imports: [
     // Aquí solo se importan otros módulos
     UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'secretKey123', // clave privada
+      signOptions: { expiresIn: '1d' }, // duración del token
+    }),
     // TypeOrmModule.forFeature([User])
   ]
 })
