dd0a8a33bdafb7aa0ff8d684b271b27340904ddf
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-15 22:00:50 -0500

    Enables login functionality
    Ensures the login route receives the DTO from the request body, allowing users to authenticate properly.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -8,16 +8,20 @@ import * as RequestWithUserInterface from 'src/interfaces/RequestWithUser.interf
 import { AuthGuard } from '@nestjs/passport';
 import { Role } from 'src/enums/user-role.enum';
 import { LoginDto } from './dto/login.dto';
+import { RolesGuard } from './guards/roles.guard';
+import { Roles } from './decorators/roles.decorator';
+import { JwtAuthGuard } from './guards/jwt.guard';
 // import { Request } from 'express';
 
 
 @Controller('auth')
+// @UseGuards(JwtAuthGuard)
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
   @Post()
-  // @UseGuards(AuthGuard, RolesGuard)
-  //  @Roles(Role.SUPER_ADMIN) // Solo el SUPER_ADMIN puede crear tiendas
+  // @UseGuards(RolesGuard)
+  // @Roles(Role.SUPER_ADMIN) // Solo el SUPER_ADMIN puede crear tiendas
   async createStore(
     @Body() createStoreDto: CreateStoreDto,
     // @Req() req: RequestWithUserInterface.RequestWithUser,
@@ -27,7 +31,7 @@ export class AuthController {
   }
 
   @Post('login')
-  async login(dto: LoginDto) {
+  async login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
   }
 
