29350b3d8456e9d4ab689282e48df047ede1a64f
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-16 19:02:52 -0500

    Changes user ID to UUID
    Updates the user ID type from number to UUID string.
    
    This change ensures a more globally unique identifier for users,
    improving data integrity and scalability.

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -7,7 +7,7 @@ import { Role } from '../../../enums/user-role.enum';
 @Entity({ name: 'users' })
 export class User {
     @PrimaryGeneratedColumn('uuid')
-    user_id: number;
+    user_id: string;
 
     @Column({ length: 250 })
     name: string;
