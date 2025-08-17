e4fc4af16f3e795bb2d454d90be9aa71af641223
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-16 22:58:50 -0500

    Configures TypeORM for Category entity
    Configures the Category module to use TypeORM
    by importing the TypeOrmModule and registering the
    Category entity for feature. This allows the module
    to interact with the database for category-related
    operations.

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { Type } from 'class-transformer';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
+import { Category } from './entities/category.entity';
 
 @Module({
   controllers: [CategoriesController],
   providers: [CategoriesService],
+  imports: [TypeOrmModule.forFeature([Category])]
 })
-export class CategoriesModule {}
+export class CategoriesModule { }
