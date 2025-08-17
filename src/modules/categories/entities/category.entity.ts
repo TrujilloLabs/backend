215cea95e647d8e225d049829052252093da3801
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-16 21:46:20 -0500

    Defines category entity
    Defines the Category entity with its properties and relationships.

--- src/modules/categories/entities/category.entity.ts
+++ src/modules/categories/entities/category.entity.ts
@@ -1,1 +1,38 @@
-export class Category {}
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    OneToMany,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('categories')
+export class Category {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 150, unique: true })
+    name: string;
+
+    @Column({ default: true })
+    isVisible: boolean;
+
+    // Relación recursiva: categoría puede tener subcategorías
+    @ManyToOne(() => Category, (category) => category.subcategories, {
+        nullable: true,
+        onDelete: 'CASCADE',
+    })
+    parentCategory: Category;
+
+    @OneToMany(() => Category, (category) => category.parentCategory)
+    subcategories: Category[];
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
+
