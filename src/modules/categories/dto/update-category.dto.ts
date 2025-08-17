d0033ce49271c7e29cce33518e713dcf1c0bdda7
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-16 21:49:03 -0500

    Updates category DTO for update operations
    Updates the `UpdateCategoryDto` to use individual property validation instead of inheriting from `CreateCategoryDto` using `PartialType`.
    This allows for more specific validation rules for each property during update operations, such as marking fields as optional.

--- src/modules/categories/dto/update-category.dto.ts
+++ src/modules/categories/dto/update-category.dto.ts
@@ -1,4 +1,18 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateCategoryDto } from './create-category.dto';
+import { IsNotEmpty, IsOptional, IsString, IsBoolean, IsUUID } from 'class-validator';
+// export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+export class UpdateCategoryDto {
+    @IsString()
+    @IsOptional()
+    name?: string;
+
+    @IsBoolean()
+    @IsOptional()
+    isVisible?: boolean;
+
+    @IsUUID()
+    @IsOptional()
+    parentCategoryId?: string;
+}
