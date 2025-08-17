45152a0a29170ca27edd36e5af85a97e1486cc5e
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-17 18:28:44 -0500

    Improves category creation process
    Enhances category creation by adding validation for duplicate names and improving parent category assignment.
    
    This prevents the creation of categories with existing names, throwing a ConflictException if a duplicate is detected.
    It also introduces a dedicated ParentCategoryFinder to locate parent categories, promoting code reusability and separation of concerns.

--- src/modules/categories/categories.service.ts
+++ src/modules/categories/categories.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
-import { Repository } from 'typeorm';
+import { QueryFailedError, Repository } from 'typeorm';
+import { ParentCategoryFinder } from './parent-category.finder';
 
 @Injectable()
 export class CategoriesService {
@@ -11,27 +12,44 @@ export class CategoriesService {
   constructor(
     @InjectRepository(Category)
     private readonly categoryRepo: Repository<Category>,
+    private readonly parentFinder: ParentCategoryFinder
   ) { }
 
 
   async create(dto: CreateCategoryDto): Promise<Category> {
-    const category = this.categoryRepo.create({
-      name: dto.name,
-      isVisible: dto.isVisible ?? true,
-    });
+    await this.validateCategoryName(dto.name);
+
+    const category = this.buildCategoryEntity(dto);
 
     if (dto.parentCategoryId) {
-      const parent = await this.categoryRepo.findOne({
-        where: { id: dto.parentCategoryId },
-      });
-      if (!parent) {
-        throw new NotFoundException('Parent category not found');
-      }
-      category.parentCategory = parent;
+      category.parentCategory = await this.parentFinder.findOrFail(dto.parentCategoryId);
     }
 
     return this.categoryRepo.save(category);
   }
+
+  private buildCategoryEntity(dto: CreateCategoryDto): Category {
+    return this.categoryRepo.create({
+      name: dto.name,
+      isVisible: dto.isVisible ?? true,
+    });
+  }
+
+  private async validateCategoryName(name: string): Promise<void> {
+    try {
+      const exists = await this.categoryRepo.existsBy({ name });
+      if (exists) {
+        throw new ConflictException('Category name already exists');
+      }
+    } catch (error) {
+      // Si hay error de TypeORM, lo relanzamos como Conflict
+      if (error instanceof QueryFailedError && error.driverError?.code === '23505') {
+        throw new ConflictException('Category name already exists');
+      }
+      throw error; // Re-lanzar otros errores
+    }
+  }
+
 
   findAll() {
     return `This action returns all categories`;
