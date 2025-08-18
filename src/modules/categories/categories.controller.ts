7ac224db5ff25a039fd11aec0735e95f48ad31b6
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-17 19:03:27 -0500

    Secures category creation endpoint
    Adds JWT authentication and role-based authorization to the category creation endpoint.
    
    This ensures that only users with the 'ADMIN_TIENDA' role can create new categories. It also retrieves the store ID from the request's user object, associating the created category with the store.

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -1,15 +1,23 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from 'src/enums/user-role.enum';
 
 @Controller('categories')
+@UseGuards(JwtAuthGuard)
 export class CategoriesController {
   constructor(private readonly categoriesService: CategoriesService) { }
 
   @Post()
-  create(@Body() dto: CreateCategoryDto) {
-    return this.categoriesService.create(dto);
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN_TIENDA)
+  create(@Body() dto: CreateCategoryDto, @Req() req) {
+    const storeId = req.user.store_id;
+    return this.categoriesService.create(dto, storeId);
   }
 
   @Get()
