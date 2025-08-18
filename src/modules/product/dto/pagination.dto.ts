04b35057eb60765f2a42f93ac79f2acc2ff71ccb
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 15:29:01 -0500

    Ensures default pagination values are Numbers
    Ensures that the `page` and `limit` properties in the PaginationDto
    have a Number type. This prevents potential issues with type
    coercion and ensures that the validation and swagger documentation
    correctly reflect the expected data type.

--- src/modules/product/dto/pagination.dto.ts
+++ src/modules/product/dto/pagination.dto.ts
@@ -3,18 +3,20 @@ import { Type } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class PaginationDto {
-    @ApiProperty({ required: false, default: 1, minimum: 1 })
+    @ApiProperty({ required: false, default: 1, minimum: 1, type: Number })
     @IsOptional()
     @Type(() => Number)
     @IsInt()
     @Min(1)
-    page?: number = 1;
+    // page?: number;
+    page = 1;
 
-    @ApiProperty({ required: false, default: 10, minimum: 1, maximum: 100 })
+    @ApiProperty({ required: false, default: 10, minimum: 1, maximum: 100, type: Number })
     @IsOptional()
     @Type(() => Number)
     @IsInt()
     @Min(1)
     @Max(100)
-    limit?: number = 10;
+    // limit?: number;
+    limit = 10;
 }
