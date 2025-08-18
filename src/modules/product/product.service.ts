93e7c936b3508b3718db43653b4683b3b101683a
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 02:10:31 -0500

    Implements product creation logic
    Implements the logic to create a new product, including validation to ensure the category belongs to the store.
    
    This prevents the creation of products with invalid categories for a given store.

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,11 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Product } from './entities/product.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(
+    @InjectRepository(Product)
+    private readonly productRepo: Repository<Product>,
+    @InjectRepository(Category)
+    private readonly categoryRepo: Repository<Category>,
+  ) { }
+
+
+
+  async productToCreate(dto: CreateProductDto, storeId: string): Promise<Product> {
+    // Validar que la categoría pertenezca a la tienda
+    const category = await this.categoryRepo.findOne({
+      where: {
+        id: dto.categoryId,
+        store: storeId
+      },
+    });
+    if (!category) {
+      throw new BadRequestException('Categoría inválida para esta tienda');
+    }
+
+    const product = this.productRepo.create({ ...dto, storeId });
+    return await this.productRepo.save(product);
   }
 
   findAll() {
