45c092ea269d5f0be99dac1c2dc4b49e96b6abb3
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 12:14:43 -0500

    Adds API documentation properties
    Adds `ApiProperty` decorators to the `CreateCategoryDto` class
    properties. This enhances API documentation and improves the
    developer experience by providing clear information about the
    expected request body structure.

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,14 +1,18 @@
 import { IsNotEmpty, IsOptional, IsString, IsBoolean, IsUUID } from 'class-validator';
+import { PartialType, ApiProperty } from '@nestjs/swagger';
 
 export class CreateCategoryDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty()
     @IsBoolean()
     @IsOptional()
     isVisible?: boolean;
 
+    @ApiProperty()
     @IsUUID()
     @IsOptional()
     parentCategoryId?: string;
