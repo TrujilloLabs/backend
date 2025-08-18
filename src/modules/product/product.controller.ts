907988c30f3fb21763c64506ab11203ae8e22f68
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 13:44:48 -0500

    Secures and enhances product endpoints
    Improves security and functionality for product management by:
    
    - Implementing JWT authentication and role-based authorization for product creation, restricting access to ADMIN_TIENDA roles.
    - Adding store ID validation to product creation, ensuring products are associated with the correct store.
    - Enhancing product retrieval to filter products by store ID, providing store-specific product lists.
    - Introducing Swagger documentation for product endpoints, improving API clarity.
    
    Secures and enhances product endpoints
    
    Improves security and functionality for product management by:
    
    - Implementing JWT authentication and role-based authorization for product creation, restricting access to ADMIN_TIENDA roles.
    - Adding store ID validation to product creation, ensuring products are associated with the correct store.
    - Enhancing product retrieval to filter products by store ID, providing store-specific product lists.
    - Introducing Swagger documentation for product endpoints, improving API clarity.

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,21 +1,62 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt.guard';
+import { RolesGuard } from '../auth/guards/roles.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from 'src/enums/user-role.enum';
+import { StoreId } from '../auth/decorators/store-id.decorator';
+import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ProductResponseDto } from './dto/product-response.dto';
+import { CreateCategoryDto } from '../categories/dto/create-category.dto';
+import { PaginatedResponseDto } from './dto/paginated-response.dto';
 
+@ApiTags('Product')
+@ApiBearerAuth()
 @Controller('product')
+@UseGuards(JwtAuthGuard)
 export class ProductController {
-  constructor(private readonly productService: ProductService) {}
+  constructor(private readonly productService: ProductService) { }
+
+
 
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  @UseGuards(RolesGuard)
+  @Roles(Role.ADMIN_TIENDA)
+  @ApiResponse({
+    status: 201,
+    description: 'Product created successfully',
+    type: ProductResponseDto,
+  })
+  @ApiResponse({ status: 400, description: 'Invalid category for this store' })
+  create(
+    @Body() dto: CreateProductDto,
+    @StoreId() storeId: string,
+    // @Req() req
+  ) {
+    return this.productService.createProduct(dto, storeId);
   }
 
   @Get()
-  findAll() {
-    return this.productService.findAll();
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de productos paginada',
+    type: PaginatedResponseDto<ProductResponseDto>,
+  })
+  findAll(
+    // @Param('storeId') id: string,
+    @StoreId() storeId: string,
+  ) {
+    return this.productService.findAllByStoreId(storeId);
   }
+
+  /// TODO :  Fin
+
+  // @Get()
+  // findAll() {
+  //   return this.productService.findAll();
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
