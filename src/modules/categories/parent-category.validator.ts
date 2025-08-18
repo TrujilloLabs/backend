3db25a07de99a43a43923eeb2b0fe449b96607da
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-17 23:06:20 -0500

    Adds parent category validation service
    Adds a service to validate parent categories.
    
    This service checks if a parent category exists within a given store,
    if it is visible, and prevents a category from being its own parent
    (commented out in this commit, will be addressed later).

---
+++ src/modules/categories/parent-category.validator.ts
@@ -0,0 +1,43 @@
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Category } from "./entities/category.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class
+    ParentCategoryValidatorService {
+    constructor(
+        @InjectRepository(Category)
+        private readonly categoryRepository: Repository<Category>,
+    ) { }
+
+    async validateAndGetParent(
+        parentId: string,
+        storeId: string
+    ): Promise<Category> {
+        const parent = await this.categoryRepository.findOneBy({
+            id: parentId,
+            store: storeId
+        });
+
+        if (!parent) {
+            throw new NotFoundException(
+                `Parent category ${parentId} not found in your store`
+            );
+        }
+
+        if (!parent.isVisible) {
+            throw new BadRequestException(
+                'Parent category must be visible'
+            );
+        }
+
+        // if (parent.id === categoryId) {
+        //     throw new BadRequestException(
+        //         'A category cannot be its own parent'
+        //     );
+        // }
+
+        return parent;
+    }
+}
