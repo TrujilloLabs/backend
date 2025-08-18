94b71cf3e216bba3e7088757a6c71a28006d8c5b
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 02:09:12 -0500

    Adds product response DTO
    Adds a DTO for representing product data in responses.
    
    This DTO includes properties like id, name, description, price,
    stock, and other relevant product information, along with decorators
    for API documentation and data transformation.

---
+++ src/modules/product/dto/product-response.dto.ts
@@ -0,0 +1,59 @@
+
+import { PartialType, ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+
+
+
+export class ProductDto {
+    @Expose()
+    @ApiProperty()
+    id: string;
+
+    @Expose()
+    @ApiProperty()
+    name: string;
+
+    @Expose()
+    @ApiProperty({ required: false })
+    description?: string;
+
+    @Expose()
+    @ApiProperty()
+    price: number;
+
+    @Expose()
+    @ApiProperty()
+    stock: number;
+
+    @Expose()
+    @ApiProperty()
+    priceCop: number;
+
+    @Expose()
+    @ApiProperty({ required: false })
+    priceUsd?: number | null;
+
+    @Expose()
+    @ApiProperty({ required: false })
+    imageUrl?: string;
+
+    @Expose()
+    @ApiProperty()
+    storeId: string;
+
+    @Expose()
+    @ApiProperty()
+    categoryId: string;
+
+    @Expose()
+    @ApiProperty()
+    isActive: boolean;
+
+    @Expose()
+    @ApiProperty()
+    createdAt: Date;
+
+    @Expose()
+    @ApiProperty()
+    updatedAt: Date;
+}
