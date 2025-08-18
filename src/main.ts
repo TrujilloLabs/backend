637d2824f863d103d5cf96679cae0cbcdba97d87
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 11:21:53 -0500

    Adds Swagger documentation to the API
    Implements Swagger to provide interactive API documentation.
    
    This allows developers to easily explore and test the API endpoints.
    Includes configuration for local, staging, and production environments, and adds bearer authentication.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { seedSuperAdmin } from './modules/seed/seed-superadmin';
 import { DataSource } from 'typeorm';
 import { AllExceptionsFilter } from './filter/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 // async function bootstrap() {
 //   const app = await NestFactory.create(AppModule);
@@ -30,6 +31,19 @@ async function bootstrap() {
 
   app.useGlobalFilters(new AllExceptionsFilter());
 
+  const config = new DocumentBuilder()
+    .setTitle('API Documentation')
+    .setDescription('API Backend  TrujilloLabs')
+    .addServer('http://localhost:5000', 'Local server')
+    .addServer('http://example.yourapi.com', 'Starging')
+    .addServer('http://production.yourapi.com', 'Production')
+    .addTag('Etiquetar API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT ?? 3000;
   await app.listen(port);
