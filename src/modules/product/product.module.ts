a0d0092ef47ca97c5b2883fd92760b6c843144cd
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 19:02:49 -0500

    Adds product validator service
    Adds the `ProductValidatorService` to the product module.
    
    This service will be responsible for validating product data
    before it is processed, ensuring data integrity and consistency.

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -4,10 +4,11 @@ import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Category } from '../categories/entities/category.entity';
+import { ProductValidatorService } from './validators/product-validator.service';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService],
+  providers: [ProductService, ProductValidatorService],
   imports: [
     TypeOrmModule.forFeature([Product, Category]),
   ]
