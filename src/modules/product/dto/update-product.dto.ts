067a54fe93cd25c365c61da6e814bf4ddc71d5a1
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-18 19:01:48 -0500

    Improves update product DTO
    Enhances the update product DTO by adding validation for name and categoryId, and excluding storeId from being updated.
    
    This ensures data integrity and prevents unintended modifications to sensitive product attributes.

--- src/modules/product/dto/update-product.dto.ts
+++ src/modules/product/dto/update-product.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateProductDto } from './create-product.dto';
+import {
+    IsOptional, IsString, IsNumber, Min, Max, IsUUID,
+    MaxLength
+} from 'class-validator';
+import { Exclude } from 'class-transformer';
 
-export class UpdateProductDto extends PartialType(CreateProductDto) {}
+export class UpdateProductDto extends PartialType(CreateProductDto) {
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsString()
+    @MaxLength(150)
+    name?: string;
+
+    @ApiPropertyOptional()
+    @IsOptional()
+    @IsUUID()
+    categoryId?: string;
+
+    // Eliminar campos no actualizables
+    @IsOptional()
+    @Exclude() // Excluir campos que no deben actualizarse
+    storeId?: never;
+}
