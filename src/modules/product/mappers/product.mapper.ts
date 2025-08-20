5dfac6a72a249932589492fd006d8dc6ef7b0e43
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-19 21:07:55 -0500

    Adds product mapper
    Adds a mapper to transform a Product entity into a ProductResponseDto.
    
    This mapper centralizes the conversion logic and ensures consistency
    in the data returned to the client. It includes mapping of the
    associated category using CategoryMapper.

---
+++ src/modules/product/mappers/product.mapper.ts
@@ -0,0 +1,24 @@
+import { CategoryMapper } from "src/modules/categories/mappers/category.mapper";
+import { ProductResponseDto } from "../dto/product-response.dto";
+import { Product } from "../entities/product.entity";
+
+
+
+export function mapToResponseDto(product: Product): ProductResponseDto {
+    return {
+        id: product.id,
+        name: product.name,
+        description: product.description,
+        price: product.price,
+        stock: product.stock,
+        priceCop: product.priceCop,
+        priceUsd: product.priceUsd,
+        imageUrl: product.imageUrl,
+        storeId: product.storeId,
+        category: CategoryMapper.toResponseDto(product.category),
+        isActive: product.isActive,
+        createdAt: product.createdAt,
+        updatedAt: product.updatedAt,
+        deletedAt: product.deletedAt,
+    };
+}
