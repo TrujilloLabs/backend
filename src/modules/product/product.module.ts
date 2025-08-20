08fcc645009e4858b9d7a67e347549a9e3eb9de8
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-19 21:08:40 -0500

    Imports CategoryMapper
    Imports CategoryMapper into the ProductModule.
    
    This is likely a preparatory step for using the CategoryMapper
    within the ProductModule, possibly for handling category-related
    data transformations or interactions.

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -5,12 +5,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Category } from '../categories/entities/category.entity';
 import { ProductValidatorService } from './validators/product-validator.service';
+import { CategoryMapper } from '../categories/mappers/category.mapper';
 
 @Module({
   controllers: [ProductController],
   providers: [ProductService, ProductValidatorService],
   imports: [
     TypeOrmModule.forFeature([Product, Category]),
-  ]
+  ],
+  // exports: [mapperToResponseDto],
 })
 export class ProductModule { }
