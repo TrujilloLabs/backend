ef0786692cb2fb072e7bafc68c8b735e896fa0ee
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-19 23:33:45 -0500

    Adds store validator to categories module
    Ensures store existence validation is available within the categories module.
    
    This change imports and integrates the authentication and users modules to facilitate store validation during category creation and management. It also registers the `StoreValidatorService` to make it available for dependency injection.

--- src/modules/categories/categories.module.ts
+++ src/modules/categories/categories.module.ts
@@ -7,18 +7,25 @@ import { CategoriesController } from './categories.controller';
 import { Category } from './entities/category.entity';
 import { ParentCategoryFinder } from './finders/parent-category.finder';
 import { ParentCategoryValidatorService } from './validators/parent-category.validator';
-import { CategoryValidatorService } from './category.validator.service';
+import { CategoryValidatorService } from './validators/category.validator.service';
+import { AuthService } from '../auth/auth.service';
+import { UsersModule } from '../users/users.module';
+import { AuthModule } from '../auth/auth.module';
+import { StoreValidatorService } from '../auth/validators/validate-store-exists.validator';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Category]),
+    UsersModule,
+    AuthModule
   ],
   controllers: [CategoriesController],
   providers: [
     CategoriesService,
     ParentCategoryFinder,
     ParentCategoryValidatorService,
-    CategoryValidatorService
+    CategoryValidatorService,
+    StoreValidatorService,
   ],
 })
 export class CategoriesModule { }
