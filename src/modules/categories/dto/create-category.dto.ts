4f74cf9a0ab75e6d7a00591ad4fce460ca7b84fe
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-19 19:50:31 -0500

    Enhances category creation DTO
    Improves the CreateCategoryDto by adding API properties for better documentation and validation.
    
    It also trims whitespace from the category name and sets default values for optional fields.

--- src/modules/categories/dto/create-category.dto.ts
+++ src/modules/categories/dto/create-category.dto.ts
@@ -1,18 +1,32 @@
 import { IsNotEmpty, IsOptional, IsString, IsBoolean, IsUUID } from 'class-validator';
 import { PartialType, ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
 
 export class CreateCategoryDto {
-    @ApiProperty()
+    @ApiProperty({
+        example: 'Electronics',
+        description: 'The name of the category'
+    })
     @IsString()
     @IsNotEmpty()
+    @Transform(({ value }) => value.trim()) //esta linea es para eliminar espacios en blanco
     name: string;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: true,
+        description: 'Indica si la categoría es visible',
+        required: false,
+        default: true
+    })
     @IsBoolean()
     @IsOptional()
     isVisible?: boolean;
 
-    @ApiProperty()
+    @ApiProperty({
+        example: 'a1b2c3d4-e5f6-7890-abcd-ef1234567890',
+        description: 'ID de la categoría padre (opcional)',
+        required: false
+    })
     @IsUUID()
     @IsOptional()
     parentCategoryId?: string;
