d1afd2905ccbad2bbd8d47ca12b79b85f49d1c2a
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-21 10:55:55 -0500

    Adds optional properties to DTO
    Extends the `UpdateSubcategoryDto` to include optional properties such as name, visibility, category ID, and store ID.
    This enhancement allows for granular updates to subcategory entities.

--- src/modules/subcategories/dto/update-subcategory.dto.ts
+++ src/modules/subcategories/dto/update-subcategory.dto.ts
@@ -1,4 +1,42 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateSubcategoryDto } from './create-subcategory.dto';
+import { IsOptional, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateSubcategoryDto extends PartialType(CreateSubcategoryDto) {}
+export class UpdateSubcategoryDto extends PartialType(CreateSubcategoryDto) {
+
+    //name
+    @ApiProperty({
+        example: 'Electronics',
+        description: 'Nombre de la subcategoría (opcional)',
+    })
+    @IsOptional()
+    name: string;
+
+    //invisible
+    @ApiProperty({
+        example: true,
+        description: 'Indica si la subcategoría es visible (opcional)',
+    })
+    @IsOptional()
+    isVisible?: boolean;
+
+    @ApiProperty({
+        example: 'a1b2c3d4-e5f6-7890-abcd-ef1234567890',
+        description: 'ID de la categoría (opcional)',
+        required: false
+    })
+    @IsUUID()
+    @IsOptional()
+    categoryId?: string;
+
+    //storeId
+    @ApiProperty({
+        example: 'a1b2c3d4-e5f6-7890-abcd-ef1234567890',
+        description: 'ID de la tienda (opcional)',
+        required: false
+    })
+    @IsUUID()
+    @IsOptional()
+    storeId: string;
+}
