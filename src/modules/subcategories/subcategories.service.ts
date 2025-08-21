4e98e9fefae4536b234a11f5b8250deae02467d3
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-20 21:31:39 -0500

    Adds subcategory creation functionality
    Implements the creation of subcategories, including validation of store existence, category association, and name uniqueness.
    It persists the new subcategory in the database and returns a response DTO. Includes methods to validate if the category exists and the entity builder

--- src/modules/subcategories/subcategories.service.ts
+++ src/modules/subcategories/subcategories.service.ts
@@ -1,11 +1,61 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateSubcategoryDto } from './dto/create-subcategory.dto';
 import { UpdateSubcategoryDto } from './dto/update-subcategory.dto';
+import { SubcategoryResponseDto } from './dto/subcategory-response.dto';
+import { StoreValidatorService } from '../auth/validators/validate-store-exists.validator';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Subcategory } from './entities/subcategory.entity';
+import { Repository } from 'typeorm';
+import { CategoryValidatorService } from '../categories/validators/category.validator.service';
+import { CreateSubcategoryFinder } from './create-subcategory.finder';
+import { CategoryMapper } from '../categories/mappers/category.mapper';
+import { SubcategoryMapper } from './mappers/subcategory.mapper';
+import { Category } from '../categories/entities/category.entity';
 
 @Injectable()
 export class SubcategoriesService {
-  create(createSubcategoryDto: CreateSubcategoryDto) {
-    return 'This action adds a new subcategory';
+
+  constructor(
+    @InjectRepository(Subcategory)
+    private readonly subCategoryRepository: Repository<Subcategory>,
+    @InjectRepository(Category)
+    private readonly categoryRepository: Repository<Category>,
+    private readonly storeValidator: StoreValidatorService,
+    private readonly categoryValidatorService: CategoryValidatorService,
+
+
+  ) { }
+
+
+  async create(
+    createSubcategoryDto: CreateSubcategoryDto,
+    storeId: string): Promise<SubcategoryResponseDto> {
+    await this.storeValidator.validateStoreExists(storeId);
+
+    // Validar que la categoría exista y pertenezca a la tienda
+    const category = await this.validateCategoryExists(
+      createSubcategoryDto.categoryId,
+      storeId
+    );
+
+    // Validar nombre único
+    await this.categoryValidatorService.validateUniqueNameSubCategory(
+      createSubcategoryDto.name,
+      storeId
+    );
+
+    // Crear entidad
+    const subcategory = this.buildSubcategoryEntity(
+      createSubcategoryDto,
+      storeId,
+      category
+    );
+
+    // Guardar y retornar
+    const savedSubcategory = await this.subCategoryRepository.save(subcategory);
+    return SubcategoryMapper.toResponseDto(savedSubcategory);
+
+
   }
 
   findAll() {
@@ -22,5 +72,46 @@ export class SubcategoriesService {
 
   remove(id: number) {
     return `This action removes a #${id} subcategory`;
+  }
+
+
+
+
+
+  ///TODO :  METODOS 
+
+
+  private async validateCategoryExists(
+    categoryId: string,
+    storeId: string
+  ): Promise<Category> {
+    const category = await this.categoryRepository.findOne({
+      where: {
+        id: categoryId,
+        store: storeId
+      }
+    });
+
+    if (!category) {
+      throw new NotFoundException(
+        `Categoría con ID ${categoryId} no encontrada en la tienda ${storeId}`
+      );
+    }
+
+    return category;
+  }
+
+  private buildSubcategoryEntity(
+    dto: CreateSubcategoryDto,
+    storeId: string,
+    category: Category
+  ): Subcategory {
+    const subcategory = new Subcategory();
+    subcategory.name = dto.name;
+    subcategory.isVisible = dto.isVisible || false;
+    subcategory.category = category;
+    subcategory.store = storeId;
+
+    return subcategory;
   }
 }
