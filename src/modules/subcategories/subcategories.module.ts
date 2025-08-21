f51db9245744a7214218103a88f725bdbe411d2e
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-08-20 21:32:40 -0500

    Adds category validation and exports service
    Adds validators for category and store entities to ensure data integrity.
    
    Exports the subcategories service to be used in other modules.
    
    Registers Category entity for feature and imports CategoriesModule.

--- src/modules/subcategories/subcategories.module.ts
+++ src/modules/subcategories/subcategories.module.ts
@@ -3,13 +3,21 @@ import { SubcategoriesService } from './subcategories.service';
 import { SubcategoriesController } from './subcategories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Subcategory } from './entities/subcategory.entity';
+import { Category } from '../categories/entities/category.entity';
+import { Store } from '../stores/entities/store.entity';
+import { CategoryValidatorService } from './validators/category-validator.service';
+import { StoreValidatorService } from '../auth/validators/validate-store-exists.validator';
+import { CategoriesModule } from '../categories/categories.module';
 
 @Module({
   controllers: [SubcategoriesController],
-  providers: [SubcategoriesService],
+  providers: [SubcategoriesService, CategoryValidatorService, StoreValidatorService],
   imports: [
-    TypeOrmModule.forFeature([Subcategory]),
+    TypeOrmModule.forFeature([Subcategory, Category]),
+    CategoriesModule,
+
 
   ],
+  exports: [SubcategoriesService],
 })
 export class SubcategoriesModule { }
