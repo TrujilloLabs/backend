cbf24f550640568db051151e6d2d9188ab9e63d4
Diego Trujillo <diegotrujillo0210@gmail.com>
2025-09-06 10:23:15 -0500

    feat: configurando las cors

--- src/main.ts
+++ src/main.ts
@@ -38,6 +38,13 @@ async function bootstrap() {
     }
   })
 
+  // Habilitar CORS
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://localhost:5173'], // dominios del frontend
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // si necesitas enviar cookies o auth headers
+  });
+
 
 
 
